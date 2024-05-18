969cf507f9d10da7627369be5bd4deb6d47d994e
shivam222343 <164057678+shivam222343@users.noreply.github.com>
2024-05-19 02:30:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let popup = document.getElementById("popup");
 let menu = document.getElementById("menubar");
 let closer = document.getElementById("closer");
+let closer1 = document.getElementById("closer1");
+let box1 = document.getElementById("box1");
+let detpop = document.getElementById("det-pop");
 menu.onclick = function(){
     popup.style.display = "block";
     menu.style.display = "none";
@@ -9,4 +12,13 @@ menu.onclick = function(){
 closer.onclick = function(){
     popup.style.display = "none";
     menu.style.display = "block";
+}
+
+det.onclick = function(){
+    box1.style.display = "none";
+    detpop.style.display = "block";
+}
+closer1.onclick = function(){
+    detpop.style.display = "none";
+    box1.style.display = "block";
 }
