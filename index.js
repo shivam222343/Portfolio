5f503e624234d6196e010a85a34b774aec3f9ecd
shivam222343 <164057678+shivam222343@users.noreply.github.com>
2024-05-22 19:02:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,23 @@ let closer = document.getElementById("closer");
 let closer1 = document.getElementById("closer1");
 let box1 = document.getElementById("box1");
 let detpop = document.getElementById("det-pop");
+let feedpop = document.getElementById("feedpop");
+let feedbackbtn1 = document.getElementById("feedback-btn");
+let submitbox = document.getElementById("submitbox");
+
 menu.onclick = function(){
     popup.style.display = "flex";
     popup.style.position ="absolute";
     menu.style.display = "none";
+}
+feedbackbtn1.onclick = function(){
+    feedpop.style.display = "flex";
+    feedbackbtn1.style.display ="none";
+}
+
+submitbox.onclick = function(){
+    feedpop.style.display = "none";
+    feedbackbtn.style.display ="flex";
 }
 
 closer.onclick = function(){
@@ -31,3 +44,5 @@ cont.addEventListener("mousemove", function(dets){
     cursor.style.left = dets.x+"px";
    cursor.style.top = dets.y+"px";
 })
+
+
